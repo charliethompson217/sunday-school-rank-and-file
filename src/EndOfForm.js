6d8a67dcee7b146929e7dc14487c630c677fb670
Charles Thompson <charlesthompson@charless-MacBook-Air>
2023-08-16 00:33:46 -0400

    fix end of form

--- src/EndOfForm.js
+++ src/EndOfForm.js
@@ -12,7 +12,7 @@ export default function EndOfForm({rankPicks, rankedRanks, filePicks}) {
   }
   return (
     <div className='end-of-form'>
-        <h3>End of Form</h3>
+        <h1>End of Form</h1>
         <p>Thank you for making your picks. They are below. Feel free to fill out the form as many times as you'd like. Only your most recent picks will be tabulated.</p>
         <h1>Ranked Picks</h1>
         <ul>
@@ -27,8 +27,8 @@ export default function EndOfForm({rankPicks, rankedRanks, filePicks}) {
           </li>
         ))}
         </ul>
-        <ul>
         <h1>File Picks</h1>
+        <ul>
         {filePicks.map((pick, index) => (
           <li key={index}>
             {` ${filePicks[index].value} (vs ${keepLastWord(filePicks[index].game.find(
