f4f3327895095faa8479fad20f4b64e04fe5b77c
Charles Thompson <80548854+charliethompson217@users.noreply.github.com>
2024-09-14 07:27:00 -0400

    improve weekly picks

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -96,6 +96,42 @@ const Leaderboard = () => {
     return inputString;
   }
 
+  const calculateMaxColumnWidths = (players) => {
+    if (!Array.isArray(players) || players.length === 0) {
+      return { maxRankWidths: [], maxFileWidths: [] };
+    }
+    const maxRankWidths = [];
+    const maxFileWidths = [];
+
+    players.forEach((player) => {
+      const submission = filteredSubmissions[player.playerId];
+      const rankPicks = submission && typeof submission.rankPicks === 'string' ? JSON.parse(submission.rankPicks) : [];
+      const filePicks = submission && typeof submission.filePicks === 'string' ? JSON.parse(submission.filePicks) : [];
+      const rankRanks = submission && typeof submission.rankedRanks === 'string' ? JSON.parse(submission.rankedRanks) : [];
+
+      rankPicks.forEach((rank, index) => {
+        const combinedLength = (rankRanks[index]?.length || 0) + (keepLastWord(rank.value).length || 0);
+        if (!maxRankWidths[index] || combinedLength > maxRankWidths[index]) {
+          maxRankWidths[index] = combinedLength;
+        }
+      });
+
+      filePicks.forEach((file, index) => {
+        const combinedLength = keepLastWord(file.value).length;
+        if (!maxFileWidths[index] || combinedLength > maxFileWidths[index]) {
+          maxFileWidths[index] = combinedLength;
+        }
+      });
+    });
+
+    return { maxRankWidths, maxFileWidths };
+  };
+
+  const { maxRankWidths, maxFileWidths } = calculateMaxColumnWidths(sortedPlayers);
+
+  const getWidth = (length) => `${length * 10 + 15}px`;
+
+
   return (
     <div className='PlayerTable'>
       <div className='chart-tabs'>
@@ -144,128 +180,133 @@ const Leaderboard = () => {
           </tbody>
         </table>
       )}
-      
       {activeChart === 'weeklypicks' && (
-  <div>
-    <select className="week-select" value={week} onChange={(e) => setWeek(e.target.value)}>
-      {weekOptions.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
-    
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Team</th>
-            <th>Rank Picks</th>
-            <th>File Picks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedPlayers && sortedPlayers.length > 0 ? (
-            sortedPlayers.map((player, index) => {
-              const submission = filteredSubmissions[player.playerId];
-
-              const rankPicks = submission && typeof submission.rankPicks === 'string' 
-                ? JSON.parse(submission.rankPicks) 
-                : [];
-              const filePicks = submission && typeof submission.filePicks === 'string' 
-                ? JSON.parse(submission.filePicks) 
-                : [];
-              const rankRanks = submission && typeof submission.rankedRanks === 'string' 
-                ? JSON.parse(submission.rankedRanks) 
-                : [];
-
-              const currentWeekResults = gameResults[week] || {};
-              const rankResults = currentWeekResults.rankResults || [];
-              const fileResults = currentWeekResults.fileResults || [];
-
-              const isPickCorrect = (pickValue, results) => {
-                return results.some(result => keepLastWord(result.value) === pickValue);
-              };
-
-              return (
-                <tr key={index}>
-                  <td style={{ textAlign: 'center', verticalAlign: 'middle', fontWeight: 'bold' }}>{player.teamName}</td>
-                  <td>
-                    {Array.isArray(rankPicks) && rankPicks.length > 0 ? (
-                      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${rankPicks.length}, 1fr)`, gap: '2px' }}>
-                        {rankPicks.map((game, gameIndex) => {
-                          const playerPick = keepLastWord(game.value);
-                          const isCorrect = isPickCorrect(playerPick, rankResults);
-                          return (
-                            <div
-                              key={gameIndex}
-                              style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                width: '95px',
-                                height: '30px',
-                                backgroundColor: isCorrect ? 'green' : 'red',
-                                border: '1px solid black',
-                                color: 'white',
-                                fontSize: '12px',
-                              }}
-                            >
-                              {playerPick} {rankRanks[gameIndex]}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    ) : (
-                      'No Picks'
-                    )}
-                  </td>
-                  <td>
-                    {Array.isArray(filePicks) && filePicks.length > 0 ? (
-                      <div style={{ display: 'grid', gridTemplateColumns: `repeat(${filePicks.length}, 1fr)`, gap: '2px' }}>
-                        {filePicks.map((game, gameIndex) => {
-                          const playerPick = keepLastWord(game.value);
-                          const isCorrect = isPickCorrect(playerPick, fileResults);
-                          return (
-                            <div
-                              key={gameIndex}
-                              style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                width: '50px',
-                                height: '30px',
-                                backgroundColor: isCorrect ? 'green' : 'red',
-                                border: '1px solid black',
-                                color: 'white',
-                                fontSize: '12px',
-                              }}
-                            >
-                              {playerPick}
-                            </div>
-                          );
-                        })}
-                      </div>
-                    ) : (
-                      'No Picks'
-                    )}
-                  </td>
+        <div>
+          <select className="week-select" value={week} onChange={(e) => setWeek(e.target.value)}>
+            {weekOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          
+          <div>
+            <table className="weekly-picks">
+              <thead>
+                <tr>
+                  <th>Team</th>
+                  <th>Rank Picks</th>
+                  <th>File Picks</th>
                 </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td colSpan="3">No players available</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  </div>
-)}
+              </thead>
+              <tbody>
+                {sortedPlayers && sortedPlayers.length > 0 ? (
+                  sortedPlayers.map((player, index) => {
+                    const submission = filteredSubmissions[player.playerId];
+                    const rankPicks = submission && typeof submission.rankPicks === 'string' 
+                      ? JSON.parse(submission.rankPicks) 
+                      : [];
+                    const filePicks = submission && typeof submission.filePicks === 'string' 
+                      ? JSON.parse(submission.filePicks) 
+                      : [];
+                    const rankRanks = submission && typeof submission.rankedRanks === 'string' 
+                      ? JSON.parse(submission.rankedRanks) 
+                      : [];
 
+                    const currentWeekResults = gameResults[week] || {};
+                    const rankResults = currentWeekResults.rankResults || [];
+                    const fileResults = currentWeekResults.fileResults || [];
 
+                    const isPickCorrect = (pickValue, results) => {
+                      return results.some(result => keepLastWord(result.value) === pickValue);
+                    };
 
+                    return (
+                      <tr key={index}>
+                        <td style={{ textAlign: 'center', verticalAlign: 'middle', fontWeight: 'bold' }}>{player.teamName}</td>
+                        <td>
+                          {Array.isArray(rankPicks) && rankPicks.length > 0 ? (
+                            <div style={{
+                              display: 'grid',
+                              gridTemplateColumns: rankPicks.map((_, gameIndex) => getWidth(maxRankWidths[gameIndex])).join(' '), // Dynamically set column widths
+                              gap: '0px'
+                            }}>
+                              {rankPicks.map((game, gameIndex) => {
+                                const playerPick = keepLastWord(game.value);
+                                const isCorrect = isPickCorrect(playerPick, rankResults);
+                                return (
+                                  <div
+                                    key={gameIndex}
+                                    style={{
+                                      display: 'flex',
+                                      justifyContent: 'center',
+                                      alignItems: 'center',
+                                      width: getWidth(maxRankWidths[gameIndex]), // Use calculated width
+                                      height: '40px',
+                                      backgroundColor: isCorrect ? 'rgb(0, 120, 0)' : 'rgb(200, 0, 0)',
+                                      color: 'black',
+                                      fontSize: '12px',
+                                      fontWeight: 'bold',
+                                      border: '1px solid black',
+                                    }}
+                                  >
+                                    {rankRanks[gameIndex]}~{playerPick}
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          ) : (
+                            'No Picks'
+                          )}
+                        </td>
+                        <td>
+                          {Array.isArray(filePicks) && filePicks.length > 0 ? (
+                            <div style={{
+                              display: 'grid',
+                              gridTemplateColumns: filePicks.map((_, gameIndex) => getWidth(maxFileWidths[gameIndex])).join(' '), // Dynamically set column widths
+                              gap: '0px'
+                            }}>
+                              {filePicks.map((game, gameIndex) => {
+                                const playerPick = keepLastWord(game.value);
+                                const isCorrect = isPickCorrect(playerPick, fileResults);
+                                return (
+                                  <div
+                                    key={gameIndex}
+                                    style={{
+                                      display: 'flex',
+                                      justifyContent: 'center',
+                                      alignItems: 'center',
+                                      width: getWidth(maxFileWidths[gameIndex]), // Use calculated width
+                                      height: '40px',
+                                      backgroundColor: isCorrect ? 'rgb(0, 120, 0)' : 'rgb(200, 0, 0)',
+                                      color: 'black',
+                                      fontSize: '12px',
+                                      fontWeight: 'bold',
+                                      border: '1px solid black',
+                                    }}
+                                  >
+                                    {playerPick}
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          ) : (
+                            'No Picks'
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan="3">No players available</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
